2481fa91c2d62053a4366080ff7471df29c4a496
vigle7 <vigle7@gmail.com>
2018-06-28 15:12:18 +0800

    熱更新切換頁面
    首頁判斷是否可熱更新來切換頁面

--- src/ReTacToe.js
+++ src/ReTacToe.js
@@ -3,25 +3,72 @@ import {
   AppRegistry,
   Text,
   View,
-  StatusBar
+  StatusBar,
+  TouchableOpacity,
+  Button
 } from 'react-native';
 import Board from './Board';
 import styles from './Style';
 import codePush from "react-native-code-push";
 
+let codePushOptions = { checkFrequency: codePush.CheckFrequency.MANUAL };
+
 class ReTacToe extends Component {
+  constructor() {
+    super()
+    this.state = {
+      checkUp: false
+    }
+    this.onButtonPress = this.onButtonPress.bind(this)
+  }
+
+  componentDidMount() {
+    codePush.checkForUpdate()
+      .then((update) => {
+        if (!update) {
+          console.log("The app is up to date!");
+        } else {
+          this.setState({ checkUp: true })
+        }
+      });
+  }
+
+  onButtonPress() {
+    codePush.sync({
+      updateDialog: false,
+      installMode: codePush.InstallMode.IMMEDIATE
+    });
+  }
+
   render() {
     return (
-      <View style={styles.rootContainer}>
-        <StatusBar backgroundColor="#0077c2" />
-        <View style={styles.displayContainer}>
-          <Text style={styles.appsTitle}>React Native Tic Tac Toe</Text>
-          <Board />
-        </View>
-      </View>
+      (
+        this.state.checkUp ?
+          <View style={{ marginTop: 300 }}>
+            <Button
+              onPress={this.onButtonPress}
+              title="檢查更新"
+              style={{
+                backgroundColor: "blue",
+                width: 300,
+                height: 45,
+                borderWidth: 1,
+                borderRadius: 5
+              }}
+            />
+          </View>
+          :
+          <View style={styles.rootContainer}>
+            <StatusBar backgroundColor="#0077c2" />
+            <View style={styles.displayContainer}>
+              <Text style={styles.appsTitle}>React Native Tic Tac Toe</Text>
+              <Board />
+            </View>
+          </View>
+      )
     );
   }
 }
 
-ReTacToe = codePush(ReTacToe);
+ReTacToe = codePush(codePushOptions)(ReTacToe);
 AppRegistry.registerComponent('ReTacToe', () => ReTacToe);
